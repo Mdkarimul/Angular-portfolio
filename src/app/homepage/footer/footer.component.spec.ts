8d2cb77c1426ace9cc95774f071afb382be9128c
karimul islam <7mdkarimul@gmail.com>
2024-01-05 14:16:24 +0545

    test commit

--- src/app/homepage/footer/footer.component.spec.ts
+++ src/app/homepage/footer/footer.component.spec.ts
@@ -8,9 +8,9 @@ describe('FooterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-<<<<<<< HEAD
+
     declarations: [FooterComponent]
-=======
+
     imports: [FooterComponent]
 >>>>>>> 39544537facb2c98129f97ba225b9cd092140912
 });
